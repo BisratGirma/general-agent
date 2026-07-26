2a4fa427497636b05216d88a5c51b34d9e3878f6
Bisrat <bisratgirma36@gmail.com>
2026-07-26 23:03:22 +0300

    made public link availabled

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     print("Launching Gradio Interface...")
     demo.launch(
         debug=True,
-        share=False,
+        share=True,
         server_name="localhost",
         server_port=7862,
         ssr_mode=False,
